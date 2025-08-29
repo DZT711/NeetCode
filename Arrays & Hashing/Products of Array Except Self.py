dd7b182333fcf2b5244507933cdcbfd6a2c95751
nguyensihuy711 <3123411122@sv.sgu.edu.vn>
2025-08-29 07:35:34 +0000

    fix solved exercises to meet the requirements for time & space complexity

---
+++ Arrays & Hashing/Products of Array Except Self.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result=[]
+        left=1
+        right=1
+        for i in range(len(nums)):
+            result.append(left)
+            left*=nums[i]
+
+        for i in range(len(nums)-1,-1,-1):# iterate from right to left
+            result[i]*=right
+            right*=nums[i]
+        return result
+    
+s=Solution()
+print(s.productExceptSelf([1,2,3,4]))
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         result=[]
+#         for i in range(len(nums)):
+#             newarr=np.delete(nums,i)
+            
+#             result.append(int(math.prod(newarr)))
+#         return result
+# class Solution:
+#     def productExceptSelf(self, nums: List[int]) -> List[int]:
+#         result=[]
+#         for i in range(len(nums)):
+#             prod=1
+#             for j in range(len(nums)):
+#                 if i!=j:
+#                     prod*=nums[j]
+#             result.append(prod)
+#         return result
---
+++ Arrays & Hashing/Valid Anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
+        else:
+            freq=[[0]*26]
+            for i in range(len(s)):
+                freq[0][ord(s[i])-ord('a')]+=1 #Cú pháp này thường dùng để biến một ký tự thường ('a'–'z') thành chỉ số từ 0 đến 25, rất hữu ích khi làm việc với mảng đếm tần suất (frequency array) dài 26
+                freq[0][ord(t[i])-ord('a')]-=1
+            for i in range(26):
+                if freq[0][i]!=0:
+                    return False 
+            else:
+                return True
+s=Solution()
+print(s.isAnagram("anagram", "nagaram"))  # True
+print(s.isAnagram("lam", "fam"))  
+print(s.isAnagram("lam", "lam1"))  
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+
+#         return sorted(s)==sorted(t)
